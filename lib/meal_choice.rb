5dfc4736ffa022a3aaad6e35883c6d46eae8e45d
Taahira Carter <taahirac001@gmail.com>
2019-11-19 03:16:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@
 def meal_choice(veg1, veg2, protien = "meat")
   line1 = "What a nutritious meal!"
   line2 = "A plate of #{protien} with #{veg1} and #{veg2}."
-  puts line1 \n line2
+  puts line1 
 end
